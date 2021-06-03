51c9e970518495c59a5914282b445fddf3ce90da
RyanMcKelvey <ryanmckelvey98@gmail.com>
2021-06-03 15:04:17 +0100

    added basic functionality

---
+++ popup.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var submitButton = document.getElementById("submit");
+    var text =
+
+        submitButton.addEventListener('click', function() {
+            alert(document.getElementById("txt").value);
+        })
+})
